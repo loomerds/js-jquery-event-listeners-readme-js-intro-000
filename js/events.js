a8025509e1c702a38ab101a42984398cdcc79af3
Doug Loomer <loomerds@gmail.com>
2019-05-19 17:20:28 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,13 @@
 //define functions here
 
 $(document).ready(function(){
-
+  getIt();
 // call functions here
 
 });
+
+function getIt() {
+  $("p").on("click", (e)=>{
+    alert("Hey!");
+  })
+}

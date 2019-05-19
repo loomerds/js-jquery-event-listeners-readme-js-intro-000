0b2e2558ec184235a7ba133549404d9dd0fb1bfa
Doug Loomer <loomerds@gmail.com>
2019-05-19 17:47:10 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function(){
   getIt();
+  frameIt();
+  pressIt();
 // call functions here
 
 });
@@ -16,5 +18,13 @@ function frameIt() {
   $("img").on("load", ()=>{
     $("img").addClass("tasty");
     $(".tasty").css('border-color', '#0000FF');
+  })
+}
+
+function pressIt() {
+  $("form[input]").on("keydown", (e)=>{
+    if(e.key==="G") {
+      alert("You pressed G!");
+    }
   })
 }

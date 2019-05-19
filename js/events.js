41764351a743cbafc4cd4b393485988a86cc579c
Doug Loomer <loomerds@gmail.com>
2019-05-19 18:35:15 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
-
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
+function getIt() {
+  $("p").on("click", (e)=>{
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $("img").on("load", ()=>{
+    $("img").addClass("tasty");
+    $(".tasty").css('border-color', '#0000FF');
+  })
+}
+
+function pressIt() {
+  $("form #typing").on("keydown", (e)=>{
+    if(e.which===71) {
+      alert("You pressed G!");
+    }
+  })
+}
+
+function submitIt() {
+  $("form").on("submit", ()=>{
+    alert("Your form is going to be submitted now.");
+  })
+}
